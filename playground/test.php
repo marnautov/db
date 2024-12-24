68c7f990ffc34dff9f48c298419761b376bc0948
maxim <amxm@ya.ru>
2024-12-24 11:00:48 +0300

    change playground

--- playground/test.php
+++ playground/test.php
@@ -4,8 +4,12 @@ require './vendor/autoload.php';
 
 dump('Playground test..');
 
+try {
+    $db = new \Amxm\Db\PDO('mysql:dbname=test;host=127.0.0.1', 'root', '12');
+} catch(\Exception $e) {
+    die('ERROR connect to mysql: '.$e->getMessage());
+}
 
-$db = new \Amxm\Db\PDO('mysql:dbname=test;host=127.0.0.1', 'root', '12');
 
 $rows = $db->row("SELECT * FROM tests");
 dd($rows);
